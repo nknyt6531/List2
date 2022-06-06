6e816a2167b40f6eba2a49a17f21fe682a13e43a
nknyt6531 <nknyuta1991@gmail.com>
2022-06-06 08:13:48 +0000

    ログイン訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def after_sign_in_path_for(resource)
     customer_path(1)
   end
-  
+
   def after_log_in_path_for(resource)
     customer_path(1)
   end
-  
+
   def after_sign_out_path_for(resource)
-    new_user_session_path 
+    new_user_session_path
   end
 
   protected
